f2e25b9ceef3ab388c35dd81d175f5bd660306b8
Kirithikka <kirithikka@contento.io>
2024-05-26 18:48:02 +0100

    tasks index

--- resources/js/Pages/Tasks/Index.vue
+++ resources/js/Pages/Tasks/Index.vue
@@ -1,5 +1,62 @@
 <script setup>
+import { computed } from "vue";
+import { usePage } from '@inertiajs/vue3';
+
+const tasks = computed(() => usePage().props.tasks)
+
+function addNewTask() {
+    alert('clicked');
+}
+
+const PENDING = 1;
+const COMPLETED = 2;
 </script>
 <template>
-    Index!
+    <div class="font-semibold mt-10 text-2xl text-center">
+        Your tasks
+    </div>
+
+    <div class="ml-3 px-6">
+        <button type="button" class="bg-gray-800 px-2 py-2 rounded text-white" @click="addNewTask">
+            Add new task
+        </button>
+    </div>
+
+    <div>
+        <table class="border my-10 table-fixed w-full">
+            <tr class="bg-gray-200">
+                <th class="py-3">
+                    Title
+                </th>
+                <th>
+                    Description
+                </th>
+                <th>
+                    Due date
+                </th>
+                <th>
+                    Mark as completed
+                </th>
+                <th>
+                    Actions
+                </th>
+            </tr>
+
+
+            <tr v-for="task in tasks" :key="task" class="text-center">
+                <td class="py-2">{{ task.title }}</td>
+                <td>{{ task.description }}</td>
+                <td>{{ task.due_date }}</td>
+                <td>
+                    <span v-if="task.status === PENDING" class="material-icons">close</span>
+                    <span v-if="task.status === COMPLETED" class="material-icons">check</span>
+                </td>
+
+                <td>
+                    <span class="material-icons px-4">edit</span>
+                    <span class="material-icons">delete</span>
+                </td>
+            </tr>
+        </table>
+    </div>
 </template>
